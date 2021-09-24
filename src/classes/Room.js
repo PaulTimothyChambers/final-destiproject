5c4ee28f23f935fb67f4d6eab4319723a46c7be6
Paul Chambers <paultimothychambers@gmail.com>
2021-09-24 11:43:54 -0600

    Create constructor

---
+++ src/classes/Room.js
@@ -0,0 +1,11 @@
+class Room {
+  constructor(roomData) {
+    bedSize: roomData.bedSize,
+    bidet: roomData.bidet,
+    costPerNight: roomData.costPerNight,
+    numBeds: roomData.numBeds,
+    number: roomData.number,
+    roomType: roomData.roomType,
+    daysBooked: []
+  }
+}
