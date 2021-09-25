417ec2ad45b44569b51aa4cfcd12ff4ffb383551
Paul Chambers <paultimothychambers@gmail.com>
2021-09-25 14:40:11 -0600

    Create methods for login functionality

--- src/dom-manipulation.js
+++ src/dom-manipulation.js
@@ -1,4 +1,6 @@
-const guestLoginButton = document.getElementById('guestLoginButton');
+const patronLoginButton = document.getElementById('patronLoginButton');
+const usernameFieldLabel = document.getElementById('usernameFieldLabel');
+const passwordFieldLabel = document.getElementById('passwordFieldLabel');
 const usernameField = document.getElementById('usernameField');
 const passwordField = document.getElementById('passwordField');
 const submitLoginInfo = document.getElementById('submitLoginInfo');
@@ -10,30 +12,72 @@ const bottomCard = document.getElementById('bottomCard');
 let domManipulation = {
 
   displayPatronDashboard(patron) {
-
-    `<article class=" hidden">
+    topCard.innerHTML =
+    `<article class="">
       <img class="" src="" alt="">
       <p class="welcome-message">Welcome ${patron.name}</p>
     </article>`
-  }
+    domManipulation.hidePatronLogin()
+  },
+
+  hidePatronLogin() {
+    domManipulation.hide(usernameFieldLabel);
+    domManipulation.hide(passwordFieldLabel);
+    domManipulation.hide(submitLoginInfo);
+    domManipulation.hide(usernameField);
+    domManipulation.hide(passwordField);
+  },
 
   displayPatronLogin() {
-    hideElement(guestLoginButton);
-    showElement(submitLoginInfo);
-    showElement(usernameField);
-    showElement(passwordField);
-  }
+    domManipulation.hide(patronLoginButton);
+    domManipulation.show(usernameFieldLabel);
+    domManipulation.show(passwordFieldLabel);
+    domManipulation.show(submitLoginInfo);
+    domManipulation.show(usernameField);
+    domManipulation.show(passwordField);
+  },
 
-  hideElement(ele) {
+  displayPasswordErrorMessage() {
+    const password = passwordField.value;
+    if (password === 'Incorrect Password') {
+      return;
+    } else {
+      passwordField.value = 'Incorrect Password';
+      passwordField.addEventListener('click', () => domManipulation.emptyField('passwordField'));
+    }
+  },
+
+  displayUsernameErrorMessage() {
+    const name = usernameField.value;
+    if (name.startsWith('No Record of')) {
+      return;
+    } else {
+      usernameField.value = `No Record of ${name}`;
+      usernameField.addEventListener('click', () => domManipulation.emptyField('usernameField'));
+    }
+  },
+
+  emptyField(field) {
+    document.getElementById(field).value = '';
+    // submitLoginInfo.addEventListener('click', parseAllPatrons)
+  },
+
+  hide(ele) {
     ele.classList.add('hidden');
-  }
+  },
 
-  showElement(ele) {
+  show(ele) {
     ele.classList.remove('hidden');
-  }
-  guestLoginButton,
+  },
+
+  patronLoginButton,
   usernameField,
   passwordField,
-  submitLoginInfo
+  submitLoginInfo,
+  topCard,
+  leftCard,
+  rightCard,
+  bottomCard
 }
+
 export default domManipulation;
