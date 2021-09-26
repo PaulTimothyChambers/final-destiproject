58e91bb2c265dc5729714f05acdb4b0105b33ef8
Paul Chambers <paultimothychambers@gmail.com>
2021-09-25 18:25:41 -0600

    Create findTotalCostOfRooms method

--- src/classes/User.js
+++ src/classes/User.js
@@ -4,6 +4,20 @@ class Patron {
     this.name = patronsData.name;
     this.bookings = [];
   }
+
+  findTotalCostOfRooms(roomRepo) {
+    const totalCost = this.bookings.reduce((acc, booking) => {
+      roomRepo.rooms.rooms.forEach(room => {
+        if (booking.roomNumber === room.number) {
+          acc += room.costPerNight;
+        }
+        return acc
+      })
+      return acc
+    }, 0)
+    console.log('this', totalCost)
+    return totalCost
+  }
 }
 
 export default Patron;
