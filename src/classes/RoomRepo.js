a002a431a6dee966d91c3636df40e8419359cecd
Paul Chambers <paultimothychambers@gmail.com>
2021-09-26 22:41:44 -0600

    Refactor

--- src/classes/RoomRepo.js
+++ src/classes/RoomRepo.js
@@ -2,23 +2,27 @@ import Room from './Room.js';
 
 class RoomRepo {
   constructor(roomsData, bookingsData) {
-    this.rooms = roomsData;
-    this.bookings = bookingsData;
+    this.rooms = roomsData.rooms;
+    this.bookings = bookingsData.bookings;
   }
 
   findAllRoomBookings(bookingsData, patronsData) {
-    this.rooms = this.rooms.rooms.map(room => new Room(room));
+    this.rooms = this.rooms.map(room => new Room(room));
     this.rooms.forEach(room => {
       room.findSingleRoomBookings(bookingsData, this);
     })
   }
 
   findPatronBookings(patron) {
-    this.bookings.bookings.forEach(booking => {
+    this.bookings.forEach(booking => {
       if (booking.userID === patron.id){
         patron.bookings.push(booking)
       }
     })
+  }
+
+  filterRoomByType() {
+    
   }
 }
 
